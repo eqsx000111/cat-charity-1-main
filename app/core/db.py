3f8c47e51d17b2467a67cbbcfce49e93e0fea4d8
Ivan Ilnickiy <bullet_sun@mail.ru>
2026-01-08 21:16:19 +0300

    fix db.py

--- app/core/db.py
+++ app/core/db.py
@@ -1,51 +1,11 @@
-from datetime import datetime
-from typing import Optional
-
-from sqlalchemy import Boolean, DateTime, Integer
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import (
-    DeclarativeBase,
-    Mapped,
-    declared_attr,
-    mapped_column
-)
+from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
 
 class Base(DeclarativeBase):
     pass
-
-
-class AbstractBase(Base):
-
-    __abstract__ = True
-
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    full_amount: Mapped[int] = mapped_column(Integer, nullable=False)
-    invested_amount: Mapped[int] = mapped_column(
-        Integer,
-        default=0,
-        nullable=False
-    )
-    fully_invested: Mapped[bool] = mapped_column(
-        Boolean,
-        default=False,
-        nullable=False
-    )
-    create_date: Mapped[datetime] = mapped_column(
-        DateTime,
-        default=datetime.utcnow,
-        nullable=False
-    )
-    close_date: Mapped[Optional[datetime]] = mapped_column(
-        DateTime,
-        nullable=True
-    )
 
 
 engine = create_async_engine(settings.database_url)

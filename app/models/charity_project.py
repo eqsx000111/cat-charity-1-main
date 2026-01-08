9a3a7abe928958d5f75546549a1a9ad56417632f
Ivan Ilnickiy <bullet_sun@mail.ru>
2026-01-08 21:25:16 +0300

    fix pep

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -7,6 +7,3 @@ from .base import AbstractBase
 class CharityProject(AbstractBase):
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
-
-    def __repr__(self):
-        return super().__repr__()
